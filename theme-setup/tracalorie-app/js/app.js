bb97e35c17063d91c516b0cb65d6f0e29ecd5632
Kieran911 <arinzekieran@gmail.com>
2024-12-11 05:52:19 +0100

    Tracalorie App

--- theme-setup/tracalorie-app/js/app.js
+++ theme-setup/tracalorie-app/js/app.js
@@ -10,4 +10,36 @@ class CalorieTracker {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
   }
+
+  addWorkout(workout) {
+    this._workouts.push(workout);
+    this._totalCalories -= workout.calories;
+  }
 }
+
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+class workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal('Breakfast', 400);
+tracker.addMeal(breakfast);
+
+const run = new workout('Morning run', 300);
+tracker.addWorkout(run);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
