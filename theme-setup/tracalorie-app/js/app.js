35344085c1233d113e9ccff7494dd030b511970e
Kieran911 <arinzekieran@gmail.com>
2024-12-17 08:31:22 +0100

    Tracalorie App

--- theme-setup/tracalorie-app/js/app.js
+++ theme-setup/tracalorie-app/js/app.js
@@ -12,6 +12,8 @@ class CalorieTracker {
     this._displayCaloriesBurned();
     this._displayCaloriesRemaining();
     this._displayCaloriesProgress();
+
+    document.getElementById('limit').value = this._calorieLimit;
   }
 
   // Public Methods/ API
@@ -63,6 +65,7 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+    Storage.clearAll();
     this._render();
   }
 
@@ -293,6 +296,12 @@ class Storage {
     });
 
     localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+
+  static clearAll() {
+    localStorage.removeItem('totalCalories');
+    localStorage.removeItem('meals');
+    localStorage.removeItem('workouts');
   }
 }
 
